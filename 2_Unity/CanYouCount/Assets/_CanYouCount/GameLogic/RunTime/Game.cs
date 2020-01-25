ebd9ce2e4b9220bd6874f2f6dd9e746f9c8f1d53
Joshua Ansari <joshua@ansari.ca>
2020-01-25 13:36:47 -0800

    Initial code files

---
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/RunTime/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace CanYouCount
+{
+	public class Game
+	{
+		public int VisibleTileCount;
+		public int TotalTileCount;
+
+		public Tile ExpectedTile;
+
+		public Tile[] VisibleTiles;
+	}
+
+	public class Tile
+	{
+		public int MyNumber;
+	}
+}
---
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/Tests/GridTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace CanYouCount
+{
+	public class Grid_Tests
+	{
+		// Create n (game) (n # visible, m total #)
+			// We have n numbers loaded
+			// The numbers are 1..n
+		// Tile is activated
+			// Nothing happens if not the expected tile
+			// If expected tile
+				// Remove that tile from visible tiles
+				// Update expected tile to next tile
+				// Expected tile gets replaced in visible tile array
+					// If no more numbers, replace with blank
+				// Event is broadcast for replacement/hide
+				
+		// Get value
+		// 
+
+		[Test]
+		public void Test_()
+		{
+			var grid = new Grid(n, n);
+
+			
+		}
+	}
+}
