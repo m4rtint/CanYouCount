f7aa06f480f3bd9f084419b7c745879935663bc1
Martin <m4rtin.t@gmail.com>
2020-01-26 11:55:42 -0800

    Added in Base Screen

---
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameRendering/BaseScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseScreen : MonoBehaviour, CanYouCount.IScreen
+{
+    public virtual void HideScreen()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public virtual void ShowScreen()
+    {
+        gameObject.SetActive(true); 
+    }
+}
