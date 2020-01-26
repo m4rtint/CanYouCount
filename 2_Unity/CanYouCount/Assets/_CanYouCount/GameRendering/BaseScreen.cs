b0210cbbcdbbd062efe9420edecda3464c05811f
Martin <m4rtin.t@gmail.com>
2020-01-26 11:59:19 -0800

    Scale Animation for show and hide

--- 2_Unity/CanYouCount/Assets/_CanYouCount/GameRendering/BaseScreen.cs
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameRendering/BaseScreen.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public abstract class BaseScreen : MonoBehaviour, CanYouCount.IScreen
 {
+    [SerializeField]
+    private float _animationTime = 0.5f;
     public virtual void HideScreen()
     {
-        gameObject.SetActive(false);
+        ScaleAnimation(false);
     }
 
     public virtual void ShowScreen()
     {
-        gameObject.SetActive(true); 
+        ScaleAnimation(true);
+    }
+
+    private void ScaleAnimation(bool open) 
+    {
+        Vector3 scale = open ? Vector3.one : Vector3.zero;
+        transform.LeanScale(scale, _animationTime)
+              .setEase(LeanTweenType.easeOutBack)
+              .setOnComplete(() =>
+              {
+                  gameObject.SetActive(open);
+              });
     }
 }
