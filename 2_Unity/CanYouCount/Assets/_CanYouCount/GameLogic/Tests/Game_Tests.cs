755a6d206d4230abd9d9a1c1a29228a2a2783a98
Joshua Ansari <joshua@ansari.ca>
2020-01-25 16:10:11 -0800

    Added OnTileTapped Unit Tests

--- 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/Tests/Game_Tests.cs
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/Tests/Game_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace CanYouCount
 {
@@ -7,6 +8,7 @@ namespace CanYouCount
 		// Create n (game) (n # visible, m total #)
 		// We have n numbers loaded
 		// The numbers are 1..n
+
 		// Tile is activated
 		// Nothing happens if not the expected tile
 		// If expected tile
@@ -19,39 +21,157 @@ namespace CanYouCount
 		// Get value
 		// 
 
-		[Test]
-		public void Test_GameConstructor_Initial()
+		private Game SetupGameForTesting()
 		{
-			// Arrange/Act
-			const int NUM_VISIBLE = 5,
-				TOTAL_NUM = 25;
+			const int NUM_VISIBLE = 5;
+			const int TOTAL_NUM = 25;
 
 			var random = new SeededRandomService();
 			var game = new Game(random, NUM_VISIBLE, TOTAL_NUM);
 
-			// Assert
-			Assert.AreEqual(NUM_VISIBLE, game.VisibleTileCount);
-			Assert.AreEqual(NUM_VISIBLE, game.VisibleTiles.Length);
+			return game;
+		}
 
-			// Check to ensure the numbers visible are (1 to n)
-			for (int i = 1; i <= NUM_VISIBLE; i++)
+		private static Tile GetVisibleTileByValue(Game game, int tileVale)
+		{
+			for (int i = 0; i < game.VisibleTiles.Length; i++)
 			{
-				// Check if contains tile with number
-				bool found = false;
-				for (int j = 0; j < game.VisibleTileCount; j++)
+				if (game.VisibleTiles[i].TileValue == tileVale)
 				{
-					if (i == game.VisibleTiles[j].MyNumber)
-					{
-						found = true;
-						break;
-					}
-				}
-
-				if (!found)
-				{
-					Assert.Fail($"Expected Game.VisibleTiles to contain the numbers [1 to {NUM_VISIBLE}], but didn't find [{i}]");
+					return game.VisibleTiles[i];
 				}
 			}
+
+			return null;
+		}
+
+		private static Tile FindTileByPredicate(Game game, Func<Tile, bool> predicate)
+		{
+			for (int i = 0; i < game.VisibleTiles.Length; i++)
+			{
+				if (predicate.Invoke(game.VisibleTiles[i]))
+				{
+					return game.VisibleTiles[i];
+				}
+			}
+
+			return null;
+		}
+
+		private static int[] GetVisibleTileValues(Game game)
+		{
+			int[] values = new int[game.VisibleTileCount];
+			for (int i = 0; i < game.VisibleTileCount; i++)
+			{
+				originalValueOrder[i] = game.VisibleTiles[i].TileValue;
+			}
+			return values;
+		}
+
+		private static int FindTileIndexByPredicate(Game game, Func<Tile, bool> predicate)
+		{
+			for (int i = 0; i < game.VisibleTiles.Length; i++)
+			{
+				if (predicate.Invoke(game.VisibleTiles[i]))
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		[Test]
+		public void Test_GameConstructor_Initial()
+		{
+			// Arrange/Act
+			const int EXPECTED_NUM_VISIBLE = 5;
+			const int EXPECTED_TOTAL_NUM = 25;
+
+			var random = new SeededRandomService();
+			var game = new Game(random, EXPECTED_NUM_VISIBLE, EXPECTED_TOTAL_NUM);
+
+			// Assert
+			Assert.AreEqual(EXPECTED_NUM_VISIBLE, game.VisibleTileCount);
+			Assert.AreEqual(EXPECTED_TOTAL_NUM, game.VisibleTiles.Length);
+
+			// Check to ensure the numbers visible are (1 to n)
+			for (int i = 1; i <= EXPECTED_NUM_VISIBLE; i++)
+			{
+				// Try and get the desired tile
+				var tile = GetVisibleTileByValue(game, i);
+				if (tile == null)
+				{
+					Assert.Fail($"Expected Game.VisibleTiles to contain the numbers [1 to {EXPECTED_NUM_VISIBLE}], but didn't find [{i}]");
+				}
+			}
+		}
+
+		[Test]
+		public void Test_ActivateTile_FailsIfNotExpectedTile()
+		{
+			int wrongTileCount = 0;
+			void HandleWrongTile(Tile wrongTile)
+			{
+				wrongTileCount++;
+			}
+
+			int swapTileCount = 0;
+			void HandleSwapTile(Tile oldTile, Tile newTile)
+			{
+				swapTileCount++;
+			}
+
+			// Arrange
+			var game = SetupGameForTesting();
+			game.OnWrongTileTapped += HandleWrongTile;
+			game.OnCorrectTileTapped += HandleSwapTile;
+
+			// get a tile that isn't Tile 1
+			var nonExpectedTile = FindTileByPredicate(game, tile => tile.TileValue != 1);
+
+			// Act
+			game.OnTileTapped(nonExpectedTile);
+
+			// Assert
+			Assert.AreEqual(1, wrongTileCount);
+			Assert.AreEqual(0, swapTileCount);
+		}
+
+		[Test]
+		public void Test_ActivateTile_SucceedsIfExpectedTile()
+		{
+			int wrongTileCount = 0;
+			void HandleWrongTile(Tile wrongTile)
+			{
+				wrongTileCount++;
+			}
+
+			int swapTileCount = 0;
+			void HandleSwapTile(Tile oldTile, Tile newTile)
+			{
+				swapTileCount++;
+			}
+
+			// Arrange
+			var game = SetupGameForTesting();
+			game.OnWrongTileTapped += HandleWrongTile;
+			game.OnCorrectTileTapped += HandleSwapTile;
+
+			// get original tile values/ordering
+			int expectedTileIndex = FindTileIndexByPredicate(game, tile => tile.TileValue == 1);
+			Tile expectedTile = game.VisibleTiles[expectedTileIndex];
+			var originalValueOrder = GetVisibleTileValues(game);
+
+			// Act
+			game.OnTileTapped(expectedTile);
+
+			// Assert
+			Assert.AreEqual(0, wrongTileCount);
+			Assert.AreEqual(1, swapTileCount);
+
+			var updatedValueOrder = GetVisibleTileValues(game);
+			Assert.AreEqual(originalValueOrder, updatedValueOrder);
 		}
 	}
 }
