0fcfeba20969f9cfab9a34fa83c58cf85b87f372
Joshua Ansari <joshua@ansari.ca>
2020-01-25 22:36:32 -0800

    Really great shaders

--- 2_Unity/CanYouCount/Assets/_CanYouCount/GameRendering/UIManager.cs
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameRendering/UIManager.cs
@@ -3,70 +3,77 @@ using UnityEngine;
 
 namespace CanYouCount
 {
-    public class UIManager : MonoBehaviour
-    {
-        [Header("Prefabs")]
-        [SerializeField]
-        private GameObject _timerPrefab = null;
-        [SerializeField]
-        private GameObject _nextTilePrefab = null;
-        
-        private Game _game = null;
-        private TMP_Text _timerText = null;
-        private TMP_Text _nextTileText = null;
+	public class UIManager : MonoBehaviour
+	{
+		[Header("Prefabs")]
+		[SerializeField]
+		private GameObject _timerPrefab = null;
+		[SerializeField]
+		private GameObject _nextTilePrefab = null;
 
-        /// <summary>
-        /// Initialize the specified game.
-        /// </summary>
-        /// <param name="game">Game.</param>
-        public void Initialize(Game game)
-        {
-            _game = game;
-            SetupTimerUI();
-            SetupNextTileUI();
-            _game.OnCorrectTileTapped += _game_OnCorrectTileTapped;
-        }
+		private Game _game = null;
+		private TMP_Text _timerText = null;
+		private TMP_Text _nextTileText = null;
 
-        /// <summary>
-        /// Updates the user interface.
-        /// </summary>
-        public void UpdateUI()
-        {
-            _timerText.text = string.Format(GameUIContent.TwoDecimalPoint, _game?.Timer);
-        }
+		/// <summary>
+		/// Initialize the specified game.
+		/// </summary>
+		/// <param name="game">Game.</param>
+		public void Initialize(Game game)
+		{
+			_game = game;
+			SetupTimerUI();
+			SetupNextTileUI();
+			_game.OnCorrectTileTapped += _game_OnCorrectTileTapped;
+		}
 
-        /// <summary>
-        /// Cleans up.
-        /// </summary>
-        public void CleanUp()
-        {
-            Destroy(_timerText.gameObject);
-            Destroy(_nextTileText.gameObject);
-        }
+		/// <summary>
+		/// Updates the user interface.
+		/// </summary>
+		public void UpdateUI()
+		{
+			_timerText.text = string.Format(GameUIContent.TwoDecimalPoint, _game?.Timer);
+		}
 
-        private void SetupNextTileUI()
-        {
-            GameObject _nextTileObj = Instantiate<GameObject>(_nextTilePrefab, transform.parent);
-            _nextTileText = _nextTileObj.GetComponent<TMP_Text>();
-            if (_nextTileText == null)
-            {
-                _nextTileObj.AddComponent<TMP_Text>();
-            }
-        }
+		/// <summary>
+		/// Cleans up.
+		/// </summary>
+		public void CleanUp()
+		{
+#if UNITY_EDITOR
+			if (_timerText == null || _nextTileText == null)
+			{
+				return; // Objects are already disposed
+			}
+#endif
 
-        private void SetupTimerUI()
-        {
-            GameObject _timerObj = Instantiate<GameObject>(_timerPrefab, transform.parent);
-            _timerText = _timerObj.GetComponent<TMP_Text>();
-            if (_timerText == null)
-            {
-                _timerObj.AddComponent<TMP_Text>();
-            }
-        }
+			Destroy(_timerText.gameObject);
+			Destroy(_nextTileText.gameObject);
+		}
 
-        private void _game_OnCorrectTileTapped(Tile originalTile, Tile arg2)
-        {
-            _nextTileText.text = string.Format(GameUIContent.NextTile, originalTile.TileValue + 1);
-        }
-    }
+		private void SetupNextTileUI()
+		{
+			GameObject _nextTileObj = Instantiate<GameObject>(_nextTilePrefab, transform.parent);
+			_nextTileText = _nextTileObj.GetComponent<TMP_Text>();
+			if (_nextTileText == null)
+			{
+				_nextTileObj.AddComponent<TMP_Text>();
+			}
+		}
+
+		private void SetupTimerUI()
+		{
+			GameObject _timerObj = Instantiate<GameObject>(_timerPrefab, transform.parent);
+			_timerText = _timerObj.GetComponent<TMP_Text>();
+			if (_timerText == null)
+			{
+				_timerObj.AddComponent<TMP_Text>();
+			}
+		}
+
+		private void _game_OnCorrectTileTapped(Tile originalTile, Tile arg2)
+		{
+			_nextTileText.text = string.Format(GameUIContent.NextTile, originalTile.TileValue + 1);
+		}
+	}
 }
---
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/Shaders/TileRendererShader.shader
@@ -0,0 +1,77 @@
+﻿Shader "CanYouCount/TileRendererShader"
+{
+    Properties
+    {
+        [PerRendererData] _MainTex ("Main Texture", 2D) = "white" {}
+        _Color ("Tint", Color) = (1,1,1,1)
+        _SecondaryColor ("Secondary Color", Color) = (0,0,0,0)
+    }
+    SubShader
+    {
+        Tags
+        {
+        	"Queue"="Transparent" 
+            "RenderType"="Transparent" 
+            "CanUseSpriteAtlas"="True"
+        }
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		Blend One OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            static const float PI = 3.14159265f;
+
+            float4 _Color;
+            float4 _SecondaryColor;
+            sampler2D _MainTex;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            // Program Functions
+           	v2f vert(appdata IN)
+			{
+				v2f OUT;
+				OUT.vertex = UnityObjectToClipPos(IN.vertex);
+                OUT.uv = IN.uv;
+
+				#ifdef PIXELSNAP_ON
+				OUT.vertex = UnityPixelSnap (OUT.vertex);
+				#endif
+
+				return OUT;
+			}
+
+			fixed4 frag(v2f IN) : SV_Target
+			{
+                fixed blendFactor = (sin(_Time * PI * 10) + 1) * 0.5 + 0.5;
+				fixed4 texColor = tex2D(_MainTex, IN.uv);
+                fixed4 blendedColor = _Color * blendFactor + (1 - blendFactor) * _SecondaryColor;
+                texColor *= blendedColor;
+				texColor.rgb *= texColor.a;
+				return texColor;
+			}
+
+            ENDCG
+        }
+    }
+}
