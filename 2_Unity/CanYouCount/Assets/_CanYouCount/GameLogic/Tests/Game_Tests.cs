109904093e169e6f65de04f7cec5cc542309df22
Joshua Ansari <joshua@ansari.ca>
2020-01-25 16:35:21 -0800

    Fixed tests

--- 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/Tests/Game_Tests.cs
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/Tests/Game_Tests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace CanYouCount
 {
@@ -21,65 +22,9 @@ namespace CanYouCount
 		// Get value
 		// 
 
-		private Game SetupGameForTesting()
-		{
-			const int NUM_VISIBLE = 5;
-			const int TOTAL_NUM = 25;
 
-			var random = new SeededRandomService();
-			var game = new Game(random, NUM_VISIBLE, TOTAL_NUM);
-
-			return game;
-		}
-
-		private static Tile GetVisibleTileByValue(Game game, int tileVale)
-		{
-			for (int i = 0; i < game.VisibleTiles.Length; i++)
-			{
-				if (game.VisibleTiles[i].TileValue == tileVale)
-				{
-					return game.VisibleTiles[i];
-				}
-			}
-
-			throw new Exception("Could not find tile by value");
-		}
-
-		private static Tile FindTileByPredicate(Game game, Func<Tile, bool> predicate)
-		{
-			for (int i = 0; i < game.VisibleTiles.Length; i++)
-			{
-				if (predicate.Invoke(game.VisibleTiles[i]))
-				{
-					return game.VisibleTiles[i];
-				}
-			}
-
-			throw new Exception("Could not find tile by predicate");
-		}
-
-		private static int?[] GetVisibleTileValues(Game game)
-		{
-			int?[] values = new int?[game.VisibleTileCount];
-			for (int i = 0; i < game.VisibleTileCount; i++)
-			{
-				values[i] = game.VisibleTiles[i].TileValue;
-			}
-			return values;
-		}
-
-		private static int FindTileIndexByPredicate(Game game, Func<Tile, bool> predicate)
-		{
-			for (int i = 0; i < game.VisibleTiles.Length; i++)
-			{
-				if (predicate.Invoke(game.VisibleTiles[i]))
-				{
-					return i;
-				}
-			}
-
-			return -1;
-		}
+		private const int TEST_GAME_NUM_VISIBLE = 5;
+		private const int TEST_GAME_TOTAL_NUM = 25;
 
 		[Test]
 		public void Test_GameConstructor_Initial()
@@ -93,15 +38,15 @@ namespace CanYouCount
 
 			// Assert
 			Assert.AreEqual(EXPECTED_NUM_VISIBLE, game.VisibleTileCount);
-			Assert.AreEqual(EXPECTED_TOTAL_NUM, game.VisibleTiles.Length);
+			Assert.AreEqual(EXPECTED_NUM_VISIBLE, game.VisibleTiles.Length);
 
 			// Check to ensure the numbers visible are (1 to n)
 			for (int i = 1; i <= EXPECTED_NUM_VISIBLE; i++)
 			{
 				// Try and get the desired tile
 				var tile = GetVisibleTileByValue(game, i);
-                Assert.Fail($"Expected Game.VisibleTiles to contain the numbers [1 to {EXPECTED_NUM_VISIBLE}], but didn't find [{i}]");
-				
+				Assert.Fail($"Expected Game.VisibleTiles to contain the numbers [1 to {EXPECTED_NUM_VISIBLE}], but didn't find [{i}]");
+
 			}
 		}
 
@@ -169,7 +114,120 @@ namespace CanYouCount
 			Assert.AreEqual(1, swapTileCount);
 
 			var updatedValueOrder = GetVisibleTileValues(game);
-			Assert.AreEqual(originalValueOrder, updatedValueOrder);
+			Assert.AreNotEqual(originalValueOrder, updatedValueOrder);
+
+			// change value at index in the original array and compare again
+			originalValueOrder[expectedTileIndex] = game.VisibleTileCount + 1;
+			Assert.AreEqual(updatedValueOrder, originalValueOrder);
 		}
+
+		[Test]
+		public void Test_Game_FullRun()
+		{
+			var game = SetupGameForTesting();
+
+			for (int i = 1; i <= 25; i++)
+			{
+				var tile = GetVisibleTileByValue(game, i);
+				game.OnTileTapped(tile);
+			}
+
+			// TODO: Assert game is complete
+		}
+
+		[Test]
+		public void Test_Game_ABlankTileAppeared()
+		{
+			var game = SetupGameForTesting();
+
+			// Activate tiles until the next tile should be blank
+			for (int i = 1; i <= (TEST_GAME_TOTAL_NUM - TEST_GAME_NUM_VISIBLE); i++)
+			{
+				var tile = GetVisibleTileByValue(game, i);
+				game.OnTileTapped(tile);
+			}
+
+			// Next tile should be blank
+			var nextTile = GetVisibleTileByValue(game, (TEST_GAME_TOTAL_NUM - TEST_GAME_NUM_VISIBLE) + 1);
+			game.OnTileTapped(nextTile);
+
+			var blankTile = GetVisibleTilesByValue(game, null);
+			Assert.AreEqual(1, blankTile.Count);
+		}
+
+		#region Test Helpers
+
+		private Game SetupGameForTesting()
+		{
+			var random = new SeededRandomService();
+			var game = new Game(random, TEST_GAME_NUM_VISIBLE, TEST_GAME_TOTAL_NUM);
+
+			return game;
+		}
+
+		private static Tile GetVisibleTileByValue(Game game, int? tileVale)
+		{
+			for (int i = 0; i < game.VisibleTiles.Length; i++)
+			{
+				if (game.VisibleTiles[i].TileValue == tileVale)
+				{
+					return game.VisibleTiles[i];
+				}
+			}
+
+			throw new Exception("Could not find tile by value");
+		}
+
+		private static List<Tile> GetVisibleTilesByValue(Game game, int? tileVale)
+		{
+			var tiles = new List<Tile>();
+			for (int i = 0; i < game.VisibleTiles.Length; i++)
+			{
+				if (game.VisibleTiles[i].TileValue == tileVale)
+				{
+					tiles.Add(game.VisibleTiles[i]);
+				}
+			}
+
+			return tiles;
+		}
+
+		private static Tile FindTileByPredicate(Game game, Func<Tile, bool> predicate)
+		{
+			for (int i = 0; i < game.VisibleTiles.Length; i++)
+			{
+				if (predicate.Invoke(game.VisibleTiles[i]))
+				{
+					return game.VisibleTiles[i];
+				}
+			}
+
+			throw new Exception("Could not find tile by predicate");
+		}
+
+		private static int?[] GetVisibleTileValues(Game game)
+		{
+			int?[] values = new int?[game.VisibleTileCount];
+			for (int i = 0; i < game.VisibleTileCount; i++)
+			{
+				values[i] = game.VisibleTiles[i].TileValue;
+			}
+			return values;
+		}
+
+		private static int FindTileIndexByPredicate(Game game, Func<Tile, bool> predicate)
+		{
+			for (int i = 0; i < game.VisibleTiles.Length; i++)
+			{
+				if (predicate.Invoke(game.VisibleTiles[i]))
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		#endregion
 	}
 }
---
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameRendering/TileRenderer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CanYouCount
+{
+	public class TileRenderer : MonoBehaviour
+	{
+		
+	}
+}
