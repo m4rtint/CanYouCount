2010da9aba090742d6a17a4133f403774d645afd
Martin <m4rtin.t@gmail.com>
2020-01-26 11:14:09 -0800

    Changed to Can You Count Count Down

---
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/RunTime/MainScreenTextLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+namespace CanYouCount
+{
+    public class MainScreenTextLogic
+    {
+        private int _countDownValue = 2;
+        private static string[] _canYouCount = GameUIContent.CanYouCount;
+
+        /// <summary>
+        /// Gets the count down value.
+        /// </summary>
+        /// <value>The count down value.</value>
+        public int CountDownValue => _countDownValue;
+
+        /// <summary>
+        /// Gets the count down text with.
+        /// </summary>
+        /// <returns>The count down text with.</returns>
+        /// <param name="value">Value.</param>
+        public string GetCountDownText()
+        {
+             _countDownValue = Mathf.Clamp(_countDownValue, 0, _canYouCount.Length - 1);
+            return _canYouCount[_countDownValue];
+        }
+
+        /// <summary>
+        /// Decrements the count down.
+        /// </summary>
+        /// <returns>The count down.</returns>
+        public int DecrementCountDown()
+        {
+            _countDownValue--;
+            return _countDownValue;
+        }
+
+    }
+}
