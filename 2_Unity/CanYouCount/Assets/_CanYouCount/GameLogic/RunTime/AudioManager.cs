2c0f6e5c5f915723dbb3851522e0f7177099eef2
Martin <m4rtin.t@gmail.com>
2020-01-26 16:37:05 -0800

    Creating Audio Manager

---
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/RunTime/AudioManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioModel _model;
+    private AudioSource _sfxAudioSource;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _sfxAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void SetupAudioSource()
+    {
+        _sfxAudioSource = GetComponent<AudioSource>();
+        if (_sfxAudioSource == null)
+        {
+            _sfxAudioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    private void PlayOneShot(AudioClip clip)
+    {
+        _sfxAudioSource.PlayOneShot(clip);
+    }
+
+    /// <summary>
+    /// Plaies the tile click correct.
+    /// </summary>
+    public void PlayTileClickCorrect()
+    {
+        PlayOneShot(_model.TileClickCorrect);
+    }
+
+    /// <summary>
+    /// Plaies the tile click wrong.
+    /// </summary>
+    public void PlayTileClickWrong()
+    {
+        PlayOneShot(_model.TileClickWrong);
+    }
+
+    public void PlayCountDown()
+    {
+        PlayOneShot(_model.CountDown);
+    }
+
+    public void PlayUIButtonClick()
+    {
+        PlayOneShot(_model.UIButtonClick);  
+    }
+
+    public void PlayWinState()
+    {
+        PlayOneShot(_model.WinState);
+    }
+
+    public void PlayLoseState()
+    {
+        PlayOneShot(_model.LoseState);
+    }
+
+
+}
--- 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/RunTime/AudioModel.cs
+++ 2_Unity/CanYouCount/Assets/_CanYouCount/GameLogic/RunTime/AudioModel.cs
@@ -5,7 +5,10 @@ public class AudioModel : ScriptableObject
 {
     [Header("Audio Clips")]
     [SerializeField]
-    private AudioClip _tileClick;
+    private AudioClip _tileClickCorrect;
+
+    [SerializeField]
+    private AudioClip _tileClickWrong;
 
     [SerializeField]
     private AudioClip _userInterfaceButtonClick;
@@ -18,4 +21,40 @@ public class AudioModel : ScriptableObject
 
     [SerializeField]
     private AudioClip _loseState;
+
+    /// <summary>
+    /// Gets the tile click.
+    /// </summary>
+    /// <value>The tile click.</value>
+    public AudioClip TileClickCorrect => _tileClickCorrect;
+
+    /// <summary>
+    /// Gets the tile click wrong.
+    /// </summary>
+    /// <value>The tile click wrong.</value>
+    public AudioClip TileClickWrong => _tileClickWrong;
+
+    /// <summary>
+    /// Gets the UIB utton click.
+    /// </summary>
+    /// <value>The UIB utton click.</value>
+    public AudioClip UIButtonClick => _userInterfaceButtonClick;
+
+    /// <summary>
+    /// Gets the count down.
+    /// </summary>
+    /// <value>The count down.</value>
+    public AudioClip CountDown => _countDown;
+
+    /// <summary>
+    /// Gets the state of the window.
+    /// </summary>
+    /// <value>The state of the window.</value>
+    public AudioClip WinState => _winState;
+
+    /// <summary>
+    /// Gets the state of the lose.
+    /// </summary>
+    /// <value>The state of the lose.</value>
+    public AudioClip LoseState => _loseState;
 }
